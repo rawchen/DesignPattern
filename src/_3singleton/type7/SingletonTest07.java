227b715152da44390d24a9de56e92ef1fa2f8d76
RawChen <2221999792@qq.com>
2021-07-28 22:25:20 +0800

    静态内部类单例模式

--- src/_3singleton/type7/SingletonTest07.java
+++ src/_3singleton/type7/SingletonTest07.java
@@ -2,13 +2,29 @@ package _3singleton.type7;
 
 public class SingletonTest07 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Singleton instance = Singleton.getInstance();
+				System.out.println("instance.hashCode=" + instance.hashCode());
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Singleton instance2 = Singleton.getInstance();
+				System.out.println("instance2.hashCode=" + instance2.hashCode());
+			}
+		}).start();
+
+		Class c = Singleton.class;
+		Singleton s = (Singleton) c.newInstance();
+		System.out.println(s);
+
+
 		System.out.println("使用静态内部类完成单例模式");
-		Singleton instance = Singleton.getInstance();
-		Singleton instance2 = Singleton.getInstance();
-		System.out.println(instance == instance2); // true
-		System.out.println("instance.hashCode=" + instance.hashCode());
-		System.out.println("instance2.hashCode=" + instance2.hashCode());
 
 	}
 
@@ -16,7 +32,7 @@ public class SingletonTest07 {
 
 // 静态内部类完成， 推荐使用
 class Singleton {
-	private static volatile Singleton instance;
+//	private static volatile Singleton instance;
 
 	//构造器私有化
 	private Singleton() {}
@@ -28,8 +44,8 @@ class Singleton {
 
 	//提供一个静态的公有方法，直接返回SingletonInstance.INSTANCE
 
-	public static synchronized Singleton getInstance() {
-
+	//public static synchronized Singleton getInstance() {
+	public static Singleton getInstance() {
 		return SingletonInstance.INSTANCE;
 	}
 }
